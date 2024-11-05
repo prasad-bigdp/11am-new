c2479e617a659f4cebcd9f31b6a346bc64dac56c
prasad-bigdp <bigdp585@gmail.com>
2024-11-05 12:32:21 +0530

    update

---
+++ JS/code/firstJS/script.js
@@ -0,0 +1,36 @@
+//My first singleline comment
+/*my first multi line 
+comment */
+console.log("JS is soo easy")
+var a = 20;
+console.log(a);
+var a = 15; /* redeclaration is possible in JS using var*/
+console.log(a);
+a = 45; /* reassignment is also possible in js using var */
+let b = 10;
+console.log(b);
+b = 20;  /* let cannot be redeclared but can be reassigned*/
+const c = 10;
+console.log(c);
+/* const values are constants cannot be redeclared or reassigned*/
+
+function display ()
+{
+    if (true)
+    {
+         let k = 20
+    }
+    console.log(k)
+}
+display()
+
+
+
+
+
+
+
+
+
+
+
