efce8a193fcc27f8402cf921334fc52d69abd6e0
sangram:8080(sangram) <47895139+sangram8080-india@users.noreply.github.com>
2024-11-19 23:56:45 +0530

    git commit 'update'

--- JS/projects/counter/script.js
+++ JS/projects/counter/script.js
@@ -1,17 +1,22 @@
 const myCount = document.getElementById("count");
 let c = 0;
-function incr ()
-{
-    c++;
-    myCount.textContent=c
+function incr() {
+    if (c < 21) {
+        c++;
+    } else {
+        alert('Count should be less then 20')
+    }
+    myCount.textContent = c
 }
-function decr ()
-{
-    c--;
-    myCount.textContent=c
+function decr() {
+    if (c > 0) {
+        c--;
+    } else {
+        alert("less then 0 shouldn't be")
+    }
+    myCount.textContent = c
 }
-function reset ()
-{
+function reset() {
     c = 0;
-    myCount.textContent=c
+    myCount.textContent = c
 }
