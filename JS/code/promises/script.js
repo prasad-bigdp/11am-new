112ffc23d2186b5cec83023da9c29c64e336243e
prasad-bigdp <bigdp585@gmail.com>
2024-11-30 11:17:20 +0530

    updated

---
+++ JS/code/promises/script.js
@@ -0,0 +1,65 @@
+
+// setTimeout(() => console.log("Take the chocolate"), 5000)
+let x=26
+let p = new Promise((resolve,reject) =>
+{
+    setTimeout(() =>
+    {
+        if (x > 25)
+        {
+           resolve(x)
+        }
+        else
+        {
+            reject("bye")
+        }
+    },10000)
+})
+p
+    .then((val) =>
+    {
+        val++;
+        console.log("my value is : "+val)
+    })
+    .catch((err) =>console.log("error: " + err))
+
+
+
+
+
+let y=25
+let obj = new Promise((resolve,reject) =>
+{
+    if (y < 30)
+    {
+         resolve(y)
+    }
+    else
+    {
+        reject("i'm rejecting")
+   }
+})
+// promise chaining
+obj
+    .then((res) => res*2)
+    .then((res2) => res2*2)
+    .then((res3) =>  console.log("spend: "+res3) )//spend: 80
+.catch((reason)=>console.log("reason: "+reason))
+
+
+
+
+
+
+
+// checkout(cart(product(home)))
+
+// home
+//     .then(product)
+//     .the(cart)
+//     .then(checkout)
+//     .catch()
+ 
+
+
+
